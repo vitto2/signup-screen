bfcc6ec4b9f6b4f3891fae9a53acf5f06161682a
vitto2 <vitor2071@gmail.com>
2023-02-04 20:49:07 -0300

    titulos e formulário de cadastro criados

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:signup/src/pages/login_page.dart';
+import 'package:signup/src/pages/signup_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,7 +12,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.brown,
       ),
       debugShowCheckedModeBanner: false,
-      home: const Login(),
+      home: const Signup(),
     );
   }
 }
--- lib/src/pages/signup_page.dart
+++ lib/src/pages/signup_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:signup/src/widgets/text_field_widget.dart';
+import 'package:signup/src/widgets/text_widget.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -11,6 +12,80 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(245, 248, 231, 216),
+        ),
+        height: screenSize.height,
+        width: screenSize.height,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  MyText(
+                    data: "Log in",
+                    textAlign: TextAlign.end,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black45,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      MyText(
+                        data: "Sign up",
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.9,
+                  child: MyTextFieldWidget(
+                    labelText: "Your Email",
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.9,
+                  child: MyTextFieldWidget(
+                    labelText: "Name",
+                  ),
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.9,
+                  child: MyTextFieldWidget(
+                    labelText: "Password",
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
